4b4f7e49a2aea68f62e9a14408e4ddc8097a006f
adrianperezgonzalez <62966837+adrianperezgonzalez@users.noreply.github.com>
2020-04-20 21:21:00 +0200

    Update script8.sh
    corregido

--- script8.sh
+++ script8.sh
@@ -1,1 +1,11 @@
-no tengo ni idea de hacerlo
+ip="10.1.97."
+contador=0
+
+echo "Listado de IPs:"
+for i in 'seq 2 254'; do
+  correctos = 'ping -c l $ip$i | grep received | awk '{print $4}''
+  if [ $correctos -gt 0 ]
+  then
+    echo $ip$i
+  fi
+done
